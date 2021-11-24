35cb96b89ff3b62daa8ac2646fdc3129ab1e3b66
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-11-23 21:00:31 -0600

    first excercise

---
+++ assets/js/base.js
@@ -0,0 +1,8 @@
+console.warn('hola mundo')
+let a = parseInt(prompt('Favor de capturar primer numero'))
+let b = parseInt(prompt('Favor de capturar segundo numero'))
+let resultado = ''
+
+if (a>b) {
+    resultado = a / b
+}
