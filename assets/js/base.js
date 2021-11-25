5c58dab8772d4f181a23cfff3e7a3ac1abae95c0
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-11-24 19:18:11 -0600

    segunda clase

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,16 +1,2 @@
-/*console.warn("hola mundo");*/
-let a = parseInt(prompt("Favor de capturar primer numero"));
-let b = parseInt(prompt("Favor de capturar segundo numero"));
-
-
-if (a > b) {
-    console.log(a / b)
-}
-if (a < b) {
-    console.log(a + b)
-}
-if (a == b) {
-    console.log(a * b)
-} 
 
 
