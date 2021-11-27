4e7c9a28cd581242f98bff7c27a64d371fe483fc
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-11-26 21:44:52 -0600

    exercises in class

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,83 +1,26 @@
-/*let saludo = "hola";
+//Ejercicio 1
+let word = prompt('Favor de poner la palabra a transformar').toLowerCase()
+alert(word[0].toUpperCase() + word.slice(1,word.length))
 
-for (i = 0; i < saludo.length; i++) {
-  console.log(saludo[i]);
-}*/
-/*
-let word = prompt("Capturar una palabra");
-let consonante = "";
-let vocal = "";
-word = word.toLowerCase();
-for (i = 0; i < word.length; i++) {
-  switch (word[i]) {
-    case "a":
-    case "e":
-    case "i":
-    case "o":
-    case "u":
-      vocal++;
-      break;
-    case " ":
-      break;
+//ejercicio 2
+let sentence = prompt("Por favor poner una palabra con espacios").trim().toLowerCase();
 
-    default:
-      consonante++;
-      break;
+let newSentence = " ";
+for (i = 0; i <= sentence.length - 1; i++) {
+  if (sentence[i] == sentence[0] && i == 0 || sentence[i - 1] == " ") {
+    newSentence += sentence[i].toUpperCase();
+  } else {
+    newSentence += sentence[i];
   }
 }
-console.log(`tienes ${vocal} vocales & ${consonante} consonantes`);
-*/
-/*
-let word = prompt("Favor de poner una palabra con un espacio al inicio y al final").trim().toLowerCase()
-let letterE, letterA, spaces = 0;
+console.log(newSentence);
 
-for (let i = 0; i < word.length; i++) {
-  switch (word[i]) {
-    case "a":
-    case "á":
-      letterA++;
-      break;
-    case "e":
-    case "é":
-      letterE++;
-      break;
-    case " ":
-      spaces++;
-      break;
-
-    default:
-      break;
-  }
+//ejercicio 3
+let subjects = parseInt(prompt("Favor de indicar cuantas materias tiene"));
+let totalFinal = 0,cal = 0;
+for (i = 1; i <= subjects; i++) {
+  cal = parseInt(prompt(`cuales es la calificaciones de la materia ${i}?`));
+  totalFinal += cal;
 }
+console.log(`La calificacion final es de ${totalFinal / subjects}`);
 
-console.log(
-  `tienes ${letterA} letras a & ${letterE} letras e & ${spaces} espacio en medio de las palabras`
-);*/
-/*
-let number = parseInt(prompt("Favor de poner un numero de entre el 1 al 100"));
-let result = 0;
-if (!isNaN(number) && number >= 1 && number <= 100) {
-  for (i = 1; i <= number; i++) {
-    result += i;
-  }
-} else {
-  console.warn(`Dame un numero mayor a 1 y menor o igual a 100`);
-}
-alert(`${number} \n el total es ${result}`);
-*/
-
-let word = prompt('Capturar la palabra a verficar').trim().replace(/ /gi,'')
-let i, result = 0
-  while (i<=word.length) {
-    console.log(word[i],word[word.length -i])
-    if (word[i] == word[word.length - i]) {
-      console.log(word[i],word[word.length -i])
-      result++
-    }
-    i++
-  }
-  if(result == word.length){
-    console.log(`la palabra ${word} es un palindromo`)
-  }else{
-    console.log(`la palabra ${word} no es un palindromo`)
-  }
