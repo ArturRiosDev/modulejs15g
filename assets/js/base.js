c2f9c73a9e18699fe33733243c5516a54e917659
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-11-29 19:48:16 -0600

    clase 5

---
+++ assets/js/base.js
@@ -0,0 +1,19 @@
+//funcion sin parametros
+/*
+function suma(){
+  console.log(2 + 2)
+}
+
+// funcion con parametros
+function suma(a,b){
+  console.log(a + b)
+}
+*/
+
+function printWord() {
+  let user = prompt('Favor de poner su nombre de usuario')
+  console.log(user)
+}
+
+printWord()
+
