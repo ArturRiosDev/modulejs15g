2a5bbeebded35a1421450406c53e1b1bdce26a72
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-11-29 19:59:13 -0600

    clase 5

--- assets/js/base.js
+++ assets/js/base.js
@@ -9,11 +9,25 @@ function suma(a,b){
   console.log(a + b)
 }
 */
-
+/*
 function printWord() {
   let user = prompt('Favor de poner su nombre de usuario')
   console.log(user)
 }
 
 printWord()
+*/
 
+function printWordReversed() {
+  let sentence = prompt("poner una palabra");
+  let newSentence = "";
+  for (i = 0; i <= sentence.length - 1; i++) {
+    if ((sentence[i] == sentence[0] && i == 0) || sentence[i - 1] == " ") {
+      newSentence += sentence[i].toUpperCase();
+    } else {
+      newSentence += sentence[i];
+    }
+  }
+  return console.log(newSentence);
+}
+console.log(printWordReversed());
