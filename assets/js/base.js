8732037eb26a48c27825b045f68f5c27df1a2823
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-12-01 22:02:28 -0600

    class seven

--- assets/js/base.js
+++ assets/js/base.js
@@ -18,8 +18,8 @@
 console.log( mostBigger( [1, 4, 10] ) )
  */
 
-//Como sacar el numero mas pequeño 
-const littleNumber = (arr) => {
+//Como sacar el numero mas pequeño
+/* const littleNumber = (arr) => {
 	let smallest = arr[0]
 	arr.forEach( (value, index, array) => {
 		if(smallest > value){
@@ -28,4 +28,65 @@ const littleNumber = (arr) => {
 	})
 	return smallest
 }
-console.log( littleNumber( [2,1, 4, 10] ) )
+console.log( littleNumber( [2,1, 4, 10] ) ) */
+
+//arr.map
+/* const smallerNumber = (arr) => {
+	let smaller = arr[0]
+	
+	arr.forEach( (value) => {
+		if(smaller > value){
+			smaller = value
+		}
+	})
+
+	return smaller
+}
+// console.log( smallerNumber( [5,7, 3, 4, 10] ) )
+
+
+// arr.map()
+const arrExample = [11,2,3,10,5]
+const resultado = arrExample.map( (val) => {
+	return val * 2
+})
+console.log(resultado) */
+
+// dado un array de numeros,
+// retornar un array con los numeros convertidos a string
+// Entrada : [11,2,3,10,5]
+// Salida : ['11','2','3','10','5']
+
+/* let arrEx = [11, 2, 3, 10, 5];
+let result = arrEx.map((val) => {
+  return val.toString();
+});
+
+console.log(result)
+ */
+/* const arrEx = (arr) => arr.map((val)=> val.toString())
+
+console.log(arrEx([11,2,3,10,5])) */
+
+// .map()
+// función
+// Dado unn array como parametro
+// capitalizar todos los elementos que sean strings
+// typeof variable
+// input: ['hOlA', 'mundo', 123]
+// output -> ['Hola','Mundo', 123]
+
+const capitalizeArr = (arr) => {
+  let arrcapitalized =[]
+  arr.map((val) => {
+    if (typeof val == "string") {
+      val.toLowerCase()
+      val[0].toLocaleUpperCase()
+    }
+});
+  return arr
+}
+
+
+let resultCap = capitalizeArr(["hOlA", "mundo", 123]);
+console.log(resultCap);
