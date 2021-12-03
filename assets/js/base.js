eb38feece7bba56d44b8322309b79ebfe5c99225
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-12-02 22:31:47 -0600

    class eight

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,9 +1,25 @@
 //['HOla','mUndo']
 
-const capitalizeWithMap = (arr) =>{
-	let newArr = []
-	arr.map(val=>newArr.push(val[0].toUpperCase()+val.slice(1,val.length).toLowerCase()))
-	return newArr
-}
+/* const capitalizeWithMap = (arr) => arr.map(val => `${val[0].toUpperCase() + val.slice(1).toLowerCase()}`)
 let result = capitalizeWithMap(['HolA','mUndo'])
-console.log(result)
+console.log(result) */
+
+//['hola','mundo',0,5]
+
+/* const filterStrings = (arr) =>arr.filter((val) =>typeof val === 'string')
+
+console.log(filterStrings(['hola','mundo',0,5])) */
+
+//arr.reduce()
+
+let acumulador = 0 
+let arr = [1,2,3]
+
+/* arr.forEach(element => {
+	acumulador = acumulador + element
+}) */
+let sum = arr.reduce((acc,cv,index,arr)=>{
+		console.log(acc,cv)
+		return acc + cv
+}, 0)
+console.log(sum)
