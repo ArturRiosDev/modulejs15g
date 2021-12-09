e1bfa85999fb32e9d99f52d96f57cfc41f880579
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-12-09 10:40:56 -0600

    class twelve

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -24,12 +24,46 @@ let koder = {
         this.name = objFullName[0]
         this.lastName = objFullName[1]
     },
+    setGetIMC: function(){
+        this.peso = 57 
+        this.altura = 1.50
+        return this.IMC = this.peso/this.altura**2
+    },
+    addNewProperty: function(key,value){
+        this[key] = value
+    },
 
+//homework
+    get averageKoder(){
+        return this.average
+    },
+    set averageKoder(score){
+        this.average = score
+    },
+    get imcKoder(){
+        return this.setGetIMC()
+    },
+    set imcKoder(peso){
+        return this.IMC = peso/this.altura**2
+    },
+    get scoreKoder(){
+        return this.average
+    },
+    set scoreKoder(newAverage){
+        return this.average = newAverage
+    },
 
+    set newPropertyKoder(property){
+        return this.newProperty = property
+    },
+
+        
 }
 
 // Metodos
 console.log( koder.getAge() )
+console.log( koder.setGetIMC() )
+console.log( koder.addNewProperty('nationality','Mexican') )
 
 // getters
 console.log('Edad inicial: ', koder.ageKoder)
@@ -39,6 +73,20 @@ console.log('Nombre completo: ', koder.fullName)
 koder.ageKoder = 47
 koder.fullName = ['jorge luis', 'camarillo']
 
+//1.- average
+console.log(koder.averageKoder )
+console.log(koder.averageKoder = [12,20,34])
+//2.-IMC
+console.log(koder.imcKoder)
+console.log(koder.imcKoder = 55)
+//3.- SCORE
+console.log(koder.scoreKoder)
+console.log(koder.scoreKoder = [2,3,4,5])
+//4.- newProperty
+console.log(koder.newPropertyKoder)
+console.log(koder.newPropertyKoder = 'Australian')
+
+console.log(koder)
 // getter y setter de 
 /**
  * 
