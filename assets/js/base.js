9bfaa36f624879d505969b55b721f4ec79352e05
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-12-15 22:58:45 -0600

    class seventeen

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,135 +1,166 @@
-// ejercicio
-// Agregar a 5 koders mas
-// pintarlos en el DOM
-// 1. utilizar bootstrap
-// 2. Que sea responsive
-// 3. minimo 3 cards por row
-
-let koders = [
-    {
-        name: 'Emilio',
-        age: 30,
-        city: 'Guadalajara',
-        generacion: 6,
-        typeKoder: 'javascript',
-        gender: 'Masculino',
-        avatar: 'https://picsum.photos/200/205'
-    },
-    {
-        name: 'Juan',
-        age: 30,
-        city: 'Ciudad de méxico',
-        generacion: 6,
-        typeKoder: 'iOS',
-        gender: 'Masculino',
-        avatar: 'https://picsum.photos/200/208'
-    },
-    {
-        name: 'Laura',
-        age: 30,
-        city: 'Ciudad de méxico',
-        generacion: 10,
-        typeKoder: 'Python',
-        gender: 'Femenino',
-        avatar: 'https://picsum.photos/200/209'
-    },
-    {
-        name: 'Arturo',
-        age: 25,
-        city: 'Puerto Vallarta',
-        generacion: 10,
-        typeKoder: 'iOS',
-        gender: 'Masculino',
-        avatar: 'https://picsum.photos/200/210'
-    },
-    {
-        name: 'Jose',
-        age: 25,
-        city: 'Puerto Vallarta',
-        generacion: 10,
-        typeKoder: 'Android',
-        gender: 'Masculino',
-        avatar: 'https://picsum.photos/200/211'
-    },
-    {
-        name: 'juanita',
-        age: 25,
-        city: 'Puerto Vallarta',
-        generacion: 8,
-        typeKoder: 'iOS',
-        gender: 'Femenino',
-        avatar: 'https://picsum.photos/200/215'
+//PROMESAS
+// const myPromise = new Promise();
+/* const myPromise = new Promise((resolve, reject) => {  
+    // condition
+    let condition = true  
+    if(condition == true) {    
+        resolve('Promise is resolved successfully.') 
+    } else {    
+        reject('Promise is rejected') 
     }
 
-]
+})
 
-const prinKoders = () =>{
-    console.log('corriendo')
-    layoutkoders = ''
-    koders.forEach((val)=>{
-        layoutkoders+=`
-        <div class='col-md-4 my-3'>
-        <div class="card" style="width: 18rem;">
-                    <img src="${val.avatar}" class="card-img-top p-4" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Nombre : ${val.name}</h5>
-                      <p class="card-text">Edad : ${val.age}</p>
-                      <ul>
-                      <li>Ciudad: ${val.city}</li>
-                      <li>Generacion : ${val.generacion}</li>
-                      <li>Bootcamp: ${val.typeKoder}</li>
-                      </ul>
-                    </div>
-                  </div>
-                  </div>         
-        `
-    })
-    document.querySelector('#koders__list').innerHTML = layoutkoders
-}
-
-/* let buttonCreation = document.querySelector('#button__creator')
-buttonCreation.addEventListener('click',()=>{
-    prinKoders()
+myPromise.then((response)=> {
+    console.log(typeof response)
+    console.log(response)
 }) */
 
-window.onload = prinKoders()
 
 /**
- * Browser Object Model
- * Como JS se comunica con el navegador
+ * AJAX
+ * Asynchronous JavaScript And XML
+ * Metodología
  * 
- * window:
- * window.innerHeight
- * window.innerWidth
+ * xmlhttprequest
+ * .fetch()
  * 
- * location
- * window.location.href
- * window.location.hostname
- * window.location.pathname
- * window.location.protocol
+ * https://www.javascript-coder.com/tutorials/images/ajax-block-diagram1.jpg
+ * https://jsonplaceholder.typicode.com/posts
  * 
- * Navigator
- * window.navigator.appName
- * window.navigator.appCodeName
- * window.navigator.platform
- * 
- * History
- * window.history.back()
- * window.history.forward()
- * 
- * Alert
- * window.alert()
  */
-let nextPAge = document.getElementById('next')
-let prevPage = document.getElementById('prev')
-const next = ()=>{
-    window.history.forward()
+
+// cliente
+// armar el request
+// enviar la peticion http
+// verbos http
+// POST, GET, PUT, PATCH, DELETE
+// 
+// server
+// status code
+// entender la peticion
+// armar la respuesta
+// enviar la respuesta
+
+// XMLHttpRequest()
+/**
+ * Declarar un objeto XMLHttpRequest
+ * Abrir la conexion (empezar a creat el request)
+ * Escuchar cuando el response este listo
+ * Enviar la peticion
+ * 
+ */
+
+//  Declarar un objeto XMLHttpRequest
+/* const xhttp = new XMLHttpRequest()
+// Abrir la conexion (empezar a creat el request)
+// xhttp.open(metodo, url)
+xhttp.open( "GET" , "https://jsonplaceholder.typicode.com/posts", true)
+
+// Escuchar cuando el response este listo
+xhttp.onload = function(data) {
+
+    if(data.target.status === 200){
+        let objResp = JSON.parse(data.target.response)
+        console.log(objResp)
+        // imprimir en DOM
+        let templateUser = ''
+        objResp.forEach( (post) => {
+
+            templateUser += `
+            <div class="col-12 col-md-4 mb-4">
+                <div class="card bg-light" >
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${post.id}</h6>
+                        <p class="card-text">${post.body}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        })
+        document.querySelector('.grid__posts').innerHTML = templateUser
+    }
+    
+} */
+
+// Enviar la peticion
+/* xhttp.send()
+
+console.log('espera terminada') */
+
+/**
+ * 
+ * 
+ * 
+ */
+
+// objeto a texto JSON.stringify(obj)
+// texto a objeto JSON.parse(text)
+
+// APi fetch
+
+// fetch()
+
+
+// https://jsonplaceholder.typicode.com/posts/1 {}  
+// https://jsonplaceholder.typicode.com/posts/1/comments [{},{}]
+
+// Peticiones multiples
+// https://jsonplaceholder.typicode.com/posts/1 {}  
+// https://jsonplaceholder.typicode.com/posts/1/comments [{},{}]
+
+const getData = (url, callback) => {
+    const xhttp = new XMLHttpRequest()
+    xhttp.open( "GET" , url, true)
+
+    xhttp.onload = function(data) {
+        if(data.target.status === 200) {
+            let objResp = JSON.parse(data.target.response)
+            // callback
+            callback(objResp)
+
+        }
+    }
+
+    xhttp.send()
 }
-nextPAge.addEventListener('click',next)
 
-
-const prevp = ()=>{
-    window.history.back()
+const printCardPost = (post) => {
+    console.log(post)
+    let templatePost = `
+    <div class="col-12 col-md-12 text-center">
+        <div class="card bg-light" >
+            <div class="card-body text-left">
+                <h5 class="card-title">${post.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${post.id}</h6>
+                <p class="card-text">${post.body}</p>
+                <h2>Comments</h2>
+                <div class="ctn__comments">
+                    <ul class="list-group list-group-flush list__comments">
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    document.querySelector('.grid__posts').innerHTML = templatePost
 }
-prevPage.addEventListener('click',prevp)
 
+const printCommentsPost = (arrComments) => {
+
+    let templateComments = ''
+    arrComments.forEach( (comment) => {
+        templateComments +=  `
+            <li class="list-group-item">
+                <strong>${comment.name}</strong>
+                <span class="comment__body">${comment.body}</span>  
+            </li>
+        `
+    })
+    console.log(templateComments)
+    document.querySelector('.list__comments').innerHTML = templateComments
+}
+
+getData('https://jsonplaceholder.typicode.com/posts/1', printCardPost)
+getData('https://jsonplaceholder.typicode.com/posts/1/comments', printCommentsPost)
---
+++ assets/js/homework.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Tarea 1
+ * Estudiar promesas JS
+ * Esdutiar el api fetch
+ * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+ * 
+ */
+
+/**
+ * Tarea 2
+ * Metodos (verbos http)
+ * GET
+ * 
+ * POST, PUT, PATCH, DELETE
+ */
+
+
+/**
+ * Tarea 3
+ * Crear una cuenta en firebase
+ */
+
+
+/**
+ * 
+ * Tarea 4
+ * Generar un grid de todos los usuarios 
+ * https://jsonplaceholder.typicode.com/users
+ * 
+ */
