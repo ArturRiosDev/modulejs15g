6cff6de0edb1d8f3ef1c049fa600dd3a976ce3b9
ARTURO RIOS <arturo.rioscueto@gmail.com>
2021-12-16 22:41:01 -0600

    class/class-eighteen

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,166 +1,115 @@
-//PROMESAS
-// const myPromise = new Promise();
-const myPromise = new Promise((resolve, reject) => {  
-    // condition
-    let condition = true  
-    if(condition == true) {    
-        resolve('Promise is resolved successfully.') 
-    } else {    
-        reject('Promise is rejected') 
-    }
+/* // GET
+// VERbost http (POST, PUT, PATCH,  GET,DELETE)
 
-})
-
-myPromise.then((response)=> {
-    console.log(typeof response)
-    console.log(response)
-})
-
-
-/**
- * AJAX
- * Asynchronous JavaScript And XML
- * Metodología
- * 
- * xmlhttprequest
- * .fetch()
- * 
- * https://www.javascript-coder.com/tutorials/images/ajax-block-diagram1.jpg
- * https://jsonplaceholder.typicode.com/posts
- * 
- */
-
-// cliente
-// armar el request
-// enviar la peticion http
-// verbos http
-// POST, GET, PUT, PATCH, DELETE
-// 
-// server
-// status code
-// entender la peticion
-// armar la respuesta
-// enviar la respuesta
-
-// XMLHttpRequest()
-/**
- * Declarar un objeto XMLHttpRequest
- * Abrir la conexion (empezar a creat el request)
- * Escuchar cuando el response este listo
- * Enviar la peticion
- * 
- */
-
-//  Declarar un objeto XMLHttpRequest
-/* const xhttp = new XMLHttpRequest()
-// Abrir la conexion (empezar a creat el request)
-// xhttp.open(metodo, url)
-xhttp.open( "GET" , "https://jsonplaceholder.typicode.com/posts", true)
-
-// Escuchar cuando el response este listo
-xhttp.onload = function(data) {
-
-    if(data.target.status === 200){
-        let objResp = JSON.parse(data.target.response)
-        console.log(objResp)
-        // imprimir en DOM
-        let templateUser = ''
-        objResp.forEach( (post) => {
-
-            templateUser += `
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card bg-light" >
-                    <div class="card-body">
-                        <h5 class="card-title">${post.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${post.id}</h6>
-                        <p class="card-text">${post.body}</p>
-                    </div>
-                </div>
-            </div>
-            `
-        })
-        document.querySelector('.grid__posts').innerHTML = templateUser
-    }
-    
-} */
-
-// Enviar la peticion
-/* xhttp.send()
-
-console.log('espera terminada') */
-
-/**
- * 
- * 
- * 
- */
-
-// objeto a texto JSON.stringify(obj)
-// texto a objeto JSON.parse(text)
-
-// APi fetch
-
-// fetch()
-
-
-// https://jsonplaceholder.typicode.com/posts/1 {}  
-// https://jsonplaceholder.typicode.com/posts/1/comments [{},{}]
-
-// Peticiones multiples
-// https://jsonplaceholder.typicode.com/posts/1 {}  
-// https://jsonplaceholder.typicode.com/posts/1/comments [{},{}]
-
-const getData = (url, callback) => {
+// POST
+const createUser = (objUser) =>{
     const xhttp = new XMLHttpRequest()
-    xhttp.open( "GET" , url, true)
+xhttp.open( "POST" , "https://genjs-292ac-default-rtdb.firebaseio.com/posts/.json", true)
+xhttp.onload = function(data) {
+    if(data.target.status === 200){
+        console.log(data)
+    }
+}
+}
+const xhttp = new XMLHttpRequest()
+xhttp.open( "POST" , "https://genjs-292ac-default-rtdb.firebaseio.com/posts/.json", true)
+xhttp.onload = function(data) {
+    if(data.target.status === 200){
+        console.log(data)
+    }
+    xhttp.send( JSON.stringify(objNewPost) )
+}
 
+let objNewPost =  {
+    author: 'Arturo Rios',
+    title: 'foo',
+    body: 'bar lorem',
+}
+
+//createUser()
+//xhttp.send( JSON.stringify(objNewPost) )
+
+// PATCH actualizar datos
+const updateUser =  (objUpdate, idUser) => {
+    const xhttp = new XMLHttpRequest()
+    xhttp.open( "PATCH" , `https://genjs-292ac-default-rtdb.firebaseio.com/posts/${idUser}.json`, true)
     xhttp.onload = function(data) {
-        if(data.target.status === 200) {
-            let objResp = JSON.parse(data.target.response)
-            // callback
-            callback(objResp)
-
+        if(data.target.status === 200){
+            console.log(data)
         }
     }
+    xhttp.send( JSON.stringify(objUpdate) )
+}
 
+let objUpdatePost =  {
+    author: 'Arturo Rios Cueto',
+}
+
+//updateUser(objUpdatePost, '-Mr55K9JfDNYwZC-GCsN')
+
+// DELETE Eliminar datos
+const deleteUser =  (idUser) => {
+    const xhttp = new XMLHttpRequest()
+    xhttp.open( "DELETE" , `https://genjs-292ac-default-rtdb.firebaseio.com/posts/${idUser}.json`, true)
+    xhttp.onload = function(data) {
+        if(data.target.status === 200){
+            console.log(data)
+        }
+    }
     xhttp.send()
 }
+//deleteUser('-Mr55K9JfDNYwZC-GCsN')
+ */
+// POST crear datos
+// POST crear datos
+const createUser = (objPost) => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(
+    "POST",
+    "https://arturdev-15g-default-rtdb.firebaseio.com/.json",
+    true
+  );
+  xhttp.onload = function (data) {
+    if (data.target.status === 200) {
+      document.querySelector("#title").value = "";
+      document.querySelector("#author").value = "";
+      document.querySelector("#timetoread").value = "";
+      document.querySelector("#resume").value = "";
+      let res = JSON.parse(data.target.response);
+      let idPost = res.name;
+      document.getElementById("post__id").innerText = idPost;
+      document.getElementById("alert__response").classList.remove("d-none");
+    }
+  };
+  
+  xhttp.send( JSON.stringify(objPost) )
+};
 
-const printCardPost = (post) => {
-    console.log(post)
-    let templatePost = `
-    <div class="col-12 col-md-12 text-center">
-        <div class="card bg-light" >
-            <div class="card-body text-left">
-                <h5 class="card-title">${post.title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${post.id}</h6>
-                <p class="card-text">${post.body}</p>
-                <h2>Comments</h2>
-                <div class="ctn__comments">
-                    <ul class="list-group list-group-flush list__comments">
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    `
-    document.querySelector('.grid__posts').innerHTML = templatePost
-}
+let send__post = document.querySelector("#send__post");
+send__post.addEventListener("click", () => {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let timetoread = document.querySelector("#timetoread").value;
+  let resume = document.querySelector("#resume").value;
 
-const printCommentsPost = (arrComments) => {
+  if (title === "" || author === "" || timetoread === "" || resume === "") {
+    alert("Favor de llenar todos los campos");
+  } else {
+    // console.log(title, author, timetoread, resume)
 
-    let templateComments = ''
-    arrComments.forEach( (comment) => {
-        templateComments +=  `
-            <li class="list-group-item">
-                <strong>${comment.name}</strong>
-                <span class="comment__body">${comment.body}</span>  
-            </li>
-        `
-    })
-    console.log(templateComments)
-    document.querySelector('.list__comments').innerHTML = templateComments
-}
+    let objNewPost = {
+      title,
+      author,
+      timetoread,
+      resume,
+    };
 
-getData('https://jsonplaceholder.typicode.com/posts/1', printCardPost)
-getData('https://jsonplaceholder.typicode.com/posts/1/comments', printCommentsPost)
+    createUser(objNewPost);
+  }
+});
+
+// let objNewPost =  {
+//     author: 'Jorge luis',
+//     title: 'foo',
+//     body: 'bar lorem',
+// }
